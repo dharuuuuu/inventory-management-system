a56844ce91265a615eaa8893b1313b1a8f124413
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-12-19 22:21:39 +0700

    Fix route && blade

--- resources/views/components/home/feature.blade.php
+++ resources/views/components/home/feature.blade.php
@@ -1,7 +1,8 @@
 @php
+@php
 $data1 = array(
-    'title' => 'Feature1',
-    'description' => 'Shares some characteristics of a real written text',
+    'title' => 'Jual Beli',
+    'description' => 'Transaksi jual-beli dapat dilakukan kapanpun',
     'url_page' => '/',
     'icon' => '<svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
@@ -9,8 +10,8 @@ $data1 = array(
                     </svg>'
 );
 $data2 = array(
-    'title' => 'Feature2',
-    'description' => 'Shares some characteristics of a real written text',
+    'title' => 'Simpan Barang',
+    'description' => 'Simpan dan titipkan produk pertanian di Gudang/Toko',
     'url_page' => '/',
     'icon' => '<svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M9.465 10H12a2 2 0 1 1 0 4H9.465c.34-.588.535-1.271.535-2 0-.729-.195-1.412-.535-2z"/>
@@ -19,8 +20,8 @@ $data2 = array(
                     </svg>'
 );
 $data3 = array(
-    'title' => 'Feature3',
-    'description' => 'Shares some characteristics of a real written text',
+    'title' => 'Kelola Barang',
+    'description' => 'Kelola produk yang tersimpan di Gudang dengan perawatan yang handal',
     'url_page' => '/',
     'icon' => '<svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
